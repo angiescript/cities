c0e86dacf3fa56e29b9db4dfbb46d406194d43af
Alexander Perala <alexander.perala@hotmail.com>
2021-09-09 12:37:55 +0200

    Add GET request to CitiesAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+import axios from "axios"
 import "./app.module.scss";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Main from "./Main";
 
 function App() {
+
+  
+
+const fetchData = async (query) => {
+
+  var options = {
+    method: 'GET',
+    url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities',
+    params: {
+      minPopulation: '500',
+      namePrefix: `${query}`,
+      sort: '-population ',
+      languageCode: 'en'
+    },
+    headers: {
+      'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
+      'x-rapidapi-key': 'cbdb60d271msh4d770f4189d5422p10c515jsn248e3c4f8c77'
+    }
+  };
+  
+  const request = await axios.request(options).then((response) => {
+    console.log(response.data.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+
+fetchData("paris");
+
+
+
+
   return (
     <div className="App">
       <Navbar />
