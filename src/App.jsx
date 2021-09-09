03fd914e7d483e4ec1c3532929bff8198ab3e819
Alexander Perala <alexander.perala@hotmail.com>
2021-09-09 13:30:06 +0200

    update get-request

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import axios from "axios"
+import axios from "axios";
+import { useState } from "react";
 import "./app.module.scss";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -6,36 +7,37 @@ import Main from "./Main";
 
 function App() {
 
-  
+  const [cities, setCities] = useState([]);
 
-const fetchData = async (query) => {
+  const fetchData = async (query) => {
+    var options = {
+      method: "GET",
+      url: "https://wft-geo-db.p.rapidapi.com/v1/geo/cities",
+      params: {
+        minPopulation: "500",
+        namePrefix: `${query}`,
+        sort: "-population ",
+        languageCode: "en",
+      },
+      headers: {
+        "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+        "x-rapidapi-key": "cbdb60d271msh4d770f4189d5422p10c515jsn248e3c4f8c77",
+      },
+    };
 
-  var options = {
-    method: 'GET',
-    url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities',
-    params: {
-      minPopulation: '500',
-      namePrefix: `${query}`,
-      sort: '-population ',
-      languageCode: 'en'
-    },
-    headers: {
-      'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
-      'x-rapidapi-key': 'cbdb60d271msh4d770f4189d5422p10c515jsn248e3c4f8c77'
-    }
+    const request = await axios
+      .request(options)
+      .then((response) => {
+        setCities(response.data.data);
+        console.log(response.data.data);
+        return request;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   };
-  
-  const request = await axios.request(options).then((response) => {
-    console.log(response.data.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
-}
 
-fetchData("paris");
-
-
-
+  fetchData("paris");
 
   return (
     <div className="App">
