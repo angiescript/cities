66038abc459abb00613509d68049ef0922cf4d2e
SqueakyNinja <rasmustagerud@gmail.com>
2021-09-09 10:09:23 +0200

    add packages and setup file structure

---
+++ src/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Homepage from "./Pages/Homepage";
+
+const Main = () => {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/components/Searchbar/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Searchbar = () => {
+  const variabel = "string";
+
+  return <div></div>;
+};
+
+export default Searchbar;
