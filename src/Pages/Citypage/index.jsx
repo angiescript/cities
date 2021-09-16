ac08fdb5d5bab4d33a3a057cad8dc5413f4c6678
SqueakyNinja <rasmustagerud@gmail.com>
2021-09-16 12:11:56 +0200

    fix time not showing and styling weather

--- src/Pages/Citypage/index.jsx
+++ src/Pages/Citypage/index.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.scss";
 import { useHistory, useParams } from "react-router-dom";
 import Weather from "../../components/Weather";
 
-const Citypage = (cityInfo) => {
+const Citypage = ({ cityInfo }) => {
+  const [weatherOpen, setWeatherOpen] = useState(false);
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
@@ -12,8 +13,6 @@ const Citypage = (cityInfo) => {
   let cityHeading = "Romance in the City of Lights";
   let cityDescription =
     "Paris, nicknamed the City of Light, is the capital city of France, and the largest city in France. The area is 105 square kilometres (41 square miles), and around 2.15 million people live there. If suburbs are counted, the population of the Paris area rises to 12 million people.The Seine river runs through the oldest part of Paris, and divides it into two parts, known as the Left Bank and the Right Bank. It is surrounded by many forests.";
-
-  const lat = cityInfo.latitude;
 
   console.log(cityInfo);
 
@@ -24,8 +23,8 @@ const Citypage = (cityInfo) => {
           <div>
             <img src="https://via.placeholder.com/500x200" alt="" />
           </div>
-          {/* <Weather query={cityName} /> */}
           <div className={styles.cityInfo}>
+            <Weather query={cityName} lon={cityInfo.longitude} lat={cityInfo.latitude} open={weatherOpen} />
             <h1>{cityName}</h1>
             <h2>{cityHeading}</h2>
             <p>{cityDescription}</p>
@@ -33,8 +32,8 @@ const Citypage = (cityInfo) => {
 
           <h2>What do you want to know more about?</h2>
           <div className={styles.additionalInfoBanner}>
-            <div className={styles.weatherInfo}>
-              <img src="https://via.placeholder.com/50x50" alt="" />
+            <div className={styles.weatherInfo} onClick={() => setWeatherOpen(!weatherOpen)}>
+              <img src={`http://openweathermap.org/img/wn/01d.png`} alt="" />
               <h3>Weather in {cityName}</h3>
               <p>Click here to see upcoming weather forecasts for {cityName}.</p>
             </div>
