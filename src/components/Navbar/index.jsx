52f89afa2e8b7ba28e74848cb015ded39cb77e93
Esma Klay <esma_klay@hotmail.com>
2021-09-21 13:23:46 +0200

    added a navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import styles from "./index.module.scss";
+import {Link} from 'react-router-dom';
+
 
 const Navbar = () => {
-  return <div className={styles.navbar}></div>;
+  const randomCity = "London"
+  return (
+    <div className={styles.navbar}>
+     <div className="navbar">
+        <Link to={"/"}>
+        <h4>Home</h4>
+        </Link>
+        <div className="links">
+          <Link className="randomCity" to={`/${randomCity}`}>
+          <button>random city</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  ); 
 };
 
 export default Navbar;
