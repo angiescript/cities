66f25b4e7c9fc0c3631a92544bd7d34ea7c1db2a
SqueakyNinja <rasmustagerud@gmail.com>
2021-09-24 10:17:37 +0200

    Fix random City in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import "./app.module.scss";
 import Navbar from "./components/Navbar";
 import Main from "./Main";
 import { BrowserRouter as Router } from "react-router-dom";
+import { useEffect, useState } from "react";
 // import Footer from "./components/Footer";
 
 function App() {
+  const [cityInfo, setCityInfo] = useState({});
+
   return (
     <div className="App">
       <Router>
-        <Navbar />
-        <Main />
+        <Navbar cityInfo={cityInfo} setCityInfo={setCityInfo} />
+        <Main cityInfo={cityInfo} setCityInfo={setCityInfo} />
         {/* <Footer /> */}
       </Router>
     </div>
